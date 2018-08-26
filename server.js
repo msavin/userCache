32033a597ae9d4af5c33de462e3cc029f5e8e27c
Max Savin <its@maxsavin.com>
2018-08-26 18:45:45 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 Meteor.userCache = function () {
+	var result = undefined;
 	var instance = DDP._CurrentMethodInvocation.get() || DDP._CurrentPublicationInvocation.get();
 
 	if (!instance.userId) {
-		return;
+		return result;
 	}
 
 	var connectionId = instance.connection.id;
 	var connectionData = Meteor.default_server.sessions[connectionId];
 	var collectionViews = connectionData.collectionViews.users.documents[instance.userId];
 	var data = collectionViews && collectionViews.dataByKey || [];
-	var result = undefined;
+
 
 	Object.keys(data).forEach(function (item) {
 		if (!result) {
