e73eb06a1ec66f658829169457832985f9d95351
Romain Sennat <rsennat@e-pango.com>
2019-05-16 14:34:05 +0200

    Fix Meteor 1.8 compatibility.

--- server.js
+++ server.js
@@ -3,24 +3,35 @@ var _original = Meteor.user;
 var _getCache = function () {
 	var result = undefined;
 	var instance = DDP._CurrentMethodInvocation.get() || DDP._CurrentPublicationInvocation.get();
+	var isMeteor1_8 = Meteor.release.match('1.8') !== null;
 
 	if (!instance.userId) {
 		return result;
 	}
 
 	var connectionId = instance.connection.id;
-	var connectionData = Meteor.default_server.sessions[connectionId];
-	var collectionViews = connectionData.collectionViews.users.documents[instance.userId];
+	var connectionData = isMeteor1_8 ? Meteor.default_server.sessions.get(connectionId) : Meteor.default_server.sessions[connectionId];
+	var collectionViews = isMeteor1_8 ? connectionData.collectionViews.get('users').documents.get(instance.userId) : connectionData.collectionViews.users.documents[instance.userId];
 	var data = collectionViews && collectionViews.dataByKey || [];
 
+	if (isMeteor1_8) {
+		Array.from(data.entries()).forEach(function (item) {
+		    if (!result) {
+			    result = {};
+		    }
 
-	Object.keys(data).forEach(function (item) {
-		if (!result) {
-			result = {};
-		}
+		    result[item[0]] = item[1].value;
+	    });
+	}
+	else {
+		Object.keys(data).forEach(function (item) {
+			if (!result) {
+				result = {};
+			}
 
-		result[item] = data[item][0].value;
-	});
+			result[item] = data[item][0].value;
+		});
+	}
 
 	return result;
 }
@@ -50,7 +61,7 @@ Meteor.user = function (input) {
 
 	if (typeof input === "string") {
 		input = [input];
-	} 
+	}
 
 	if (typeof input === "object") {
 		var cache = _getCache()
